75666e5e87d551ff11b8e8d7961901fc6bee6954
quevivasbien <jensenm@uchicago.edu>
2021-10-31 16:09:26 +0000

    edited cmakelists, started torch integration

---
+++ src/torch/decisionNets.cpp
@@ -0,0 +1,1 @@
+#include "decisionNets.h"
---
+++ src/torch/decisionNets.h
@@ -0,0 +1,11 @@
+#ifndef DECISION_NETS_H
+#define DECISION_NETS_H
+
+#include <torch/torch.h>
+#include <vector>
+#include <string>
+#include <memory>
+
+class Net;
+
+#endif
